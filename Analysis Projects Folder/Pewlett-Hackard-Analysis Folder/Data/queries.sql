f384cb7e73713eb0edbcf986d2f51afd07f77ece
MamidalaV <74985818+MamidalaV@users.noreply.github.com>
2021-04-07 00:45:49 -0400

    updating files to repo

---
+++ Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Data/queries.sql
@@ -0,0 +1,44 @@
+select * from departments
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date between '1952-01-01' AND '1952-12-31'
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date between '1953-01-01' AND '1953-12-31'
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date between '1954-01-01' AND '1954-12-31'
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date between '1955-01-01' AND '1955-12-31'
+
+--Retirement Eligibility projecting employees born between 1952 and 1955 will retire
+SELECT first_name, last_name
+	FROM employees
+	WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31') 
+	AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+
+
+-- Saving into a new table
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+
+SELECT * FROM retirement_info
--- Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/schema.sql
+++ Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/schema.sql
@@ -26,6 +26,15 @@ CREATE TABLE salaries (emp_no INT NOT NULL,
                         from_date DATE NOT NULL,
                         to_date DATE NOT NULL,
                         FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-                        PRIMARY KEY (emp_no)
-);
+                        PRIMARY KEY (emp_no));
+CREATE TABLE dept_emp (emp_no INT NOT NULL,
+					  dept_no varchar NOT NULL,
+					  from_date date NOT NULL,
+					  to_date date NOT NULL,
+					  FOREIGN KEY (dept_no) REFERENCES departments (dept_no));
 
+CREATE TABLE titles (emp_no INT NOT NULL,
+					  title varchar NOT NULL,
+					  from_date date NOT NULL,
+					  to_date date NOT NULL
+					 );
